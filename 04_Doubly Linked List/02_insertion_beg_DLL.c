065be718e7c74521d05aebac295da7e0c22ac515
Riku Sahu <riku.sahu237@gmail.com>
2025-10-25 22:27:55 +0530

    Add files via upload

---
+++ 04_Doubly Linked List/02_insertion_beg_DLL.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    struct node *prev;
+    int data ;
+    struct node *next;
+};
+
+struct node *add_to_empty(struct node *head,int data){
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    temp->prev = NULL;
+    temp->data = data;
+    temp->next = NULL;
+    head = temp;
+
+    return head;
+}
+
+struct node *add_at_beg(struct node *head,int data){
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    temp->prev = NULL;
+    temp->data = data;
+    temp->next = NULL;
+
+    temp->next = head;
+    head->prev = temp;
+    
+    return temp;
+}
+
+int main(){
+    struct node *head = (struct node*)malloc(sizeof(struct node));
+    head = add_to_empty(head,10);
+    head = add_at_beg(head,20);
+    head = add_at_beg(head,30);
+    head = add_at_beg(head,40);
+    head = add_at_beg(head,50);
+
+    struct node *ptr;
+    ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d ",ptr->data);
+        ptr = ptr->next;
+    }
+    
+    return 0;
+}
