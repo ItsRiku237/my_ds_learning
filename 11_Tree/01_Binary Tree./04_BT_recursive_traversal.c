1924b2b8d7cd82df526b4620098fbed2b2b7ad82
Riku Sahu <riku.sahu237@gmail.com>
2025-12-08 23:28:54 +0530

    Add files via upload

---
+++ 11_Tree/01_Binary Tree./04_BT_recursive_traversal.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    struct node* left;
+    int data;
+    struct node* right;
+};
+
+struct node* create_node(int value){
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->data = value;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+
+struct node* create_tree(){
+    int value;
+    printf("Enter the node value (-1 for no node) : ");
+    scanf("%d",&value);
+    if(value==-1)return NULL;
+
+    struct node* root = create_node(value);
+    printf("Enter left child of %d : \n",value);
+    root->left = create_tree();
+    printf("Enter right child of %d : \n",value);
+    root->right = create_tree();
+    return root;
+}
+
+void inorder(struct node* root){
+    if(root == NULL)return ;
+    inorder(root->left);
+    printf("%d ",root->data);
+    inorder(root->right);
+}
+void postorder(struct node* root){
+    if(root == NULL)return ;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ",root->data);
+}
+void preorder(struct node* root){
+    if(root == NULL)return ;
+    printf("%d ",root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main(){
+    struct node* root = NULL;
+    printf("Create a Binary Tree : \n");
+    root = create_tree();
+    printf("\nInorder traversal : ");
+    inorder(root);
+    printf("\nPostorder traversal : ");
+    postorder(root);
+    printf("\nPreorder traversal : ");
+    preorder(root);
+    return 0;
+}
