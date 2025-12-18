40624102a57b15191b5dc8c9d4f019995f362301
Riku Sahu <riku.sahu237@gmail.com>
2025-12-18 19:44:05 +0530

    Update 05_insert_bef_pos.c

--- 09_Circular Singly Linked List/05_insert_bef_pos.c
+++ 09_Circular Singly Linked List/05_insert_bef_pos.c
@@ -22,39 +22,76 @@ struct node* add_at_beg(struct node* tail, int data){
 }
 
 struct node* add_at_end(struct node* tail, int data){
+    if (tail == NULL)
+        return add_to_empty(data);
+    
     struct node* new_node = (struct node*)malloc(sizeof(struct node));
     new_node->data = data;
     new_node->next = tail->next;
     tail->next = new_node;
     tail = tail->next;
+    // tail = new_node;
+
     return tail;
 }
 
 struct node* add_aft_pos(struct node* tail , int data, int pos){
+    if (tail == NULL || pos <= 0) {
+        printf("Invalid position\n");
+        return tail;
+    }
+
     struct node* new_node = (struct node*)malloc(sizeof(struct node));
     new_node->data = data;
 
     struct node* p = tail->next;
     int i=1;
 
+    // while (pos> 1)
+    // {
+    //     p = p->next ;
+    //     pos--;
+    // }
+
     while (i<=pos-1)
     {
         p = p->next;
+        if (p == tail->next) {
+            printf("Position out of range\n");
+            return tail;
+        }
         i++;
     }
 
-    if (p->next == tail->next)
-    {
-        return add_at_end(tail,data);
-    }
-    
-    
+    // if( p = tail){
+    //     return add_at_end(tail,data);
+    // }
     new_node->next = p->next;
     p->next = new_node;
+
+    if (p == tail)
+        tail = new_node;
+
     return tail;
+
+    // if (p->next == tail->next)
+    // {
+    //     return add_at_end(tail,data);
+    // }
+    // else{
+    //     new_node->next = p->next;
+    //     p->next = new_node;
+    //     return tail;
+    // }
+
 }
 
 struct node* add_bef_pos(struct node* tail , int data, int pos){
+    if (tail == NULL || pos <= 0) {
+        printf("Invalid position\n");
+        return tail;
+    }
+
     struct node* new_node = (struct node*)malloc(sizeof(struct node));
     new_node->data = data;
 
@@ -63,13 +100,17 @@ struct node* add_bef_pos(struct node* tail , int data, int pos){
         return add_at_beg(tail,data);
     }
     
-
     struct node* p = tail->next;
     int i=1;
 
+    // while (i < pos - 1)//same as below
     while (i<=pos-2)
     {
         p = p->next;
+        if (p == tail->next) {
+            printf("Position out of range\n");
+            return tail;
+        }
         i++;
     }
     
@@ -98,4 +139,5 @@ int main(){
     tail = add_aft_pos(tail ,450,7);
     print(tail);
     return 0;
+
 }
