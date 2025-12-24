eaa6e2575ed28cbbe3b2fdcfeb4be69570409149
Riku Sahu <riku.sahu237@gmail.com>
2025-12-24 21:43:10 +0530

    Add files via upload

---
+++ 09_Circular Singly Linked List/08_delete_last_node.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+struct node* add_to_empty(int data){
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+
+    if (temp == NULL) {
+        printf("Memory allocation failed\n");
+        return NULL;
+    }
+
+    temp->data = data;
+    temp->next = temp;
+    return temp;
+}
+
+struct node* add_at_end(struct node* tail, int data){
+    if (tail == NULL)
+        return add_to_empty(data);
+    
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->data = data;
+    new_node->next = tail->next;
+    tail->next = new_node;
+    tail = tail->next;
+    // tail = new_node;
+
+    return tail;
+}
+
+struct node* create_list(struct node* tail ){
+    int i, n, data;
+    printf("Enter thr number of the linked list : ");
+    scanf("%d",&n);
+    if(n == 0 )
+        return tail;
+
+    printf("Enter the element 1 : ");
+    scanf("%d",&data);
+    tail = add_to_empty(data);
+    
+    for (int i = 1; i < n; i++)
+    {
+        printf("Enter the element %d : ",i+1);
+        scanf("%d",&data);
+        tail = add_at_end(tail,data);
+    }
+    return tail;
+}
+
+void delete_last_node(struct node** tail){
+    if (*tail == NULL) return;
+
+    if ((*tail)->next == *tail) {
+        free(*tail);
+        *tail = NULL;
+        return;
+    }
+
+    struct node* p = *tail;
+
+    while ((*tail)->next != p) {
+        *tail = (*tail)->next;
+    }
+
+    (*tail)->next = p->next;
+    free(p);
+}
+
+void print(struct node* tail){
+    struct node* p = tail->next;
+    do{
+        printf("%d ",p->data);
+        p = p->next;
+    }while(p!=tail->next);
+}
+
+int main(){
+    struct node* tail ;
+    tail = create_list(tail);
+    print(tail);
+    delete_last_node(&tail);
+    printf("\n");
+    printf("After deleting last node : ");
+    print(tail);
+    return 0;
+}
