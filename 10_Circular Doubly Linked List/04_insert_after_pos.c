497a848e36941a35fd3b82bfa67b40cdcf99c3fe
Riku Sahu <riku.sahu237@gmail.com>
2025-12-26 23:19:38 +0530

    Add files via upload

---
+++ 10_Circular Doubly Linked List/04_insert_after_pos.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    struct node* prev;
+    int data;
+    struct node* next;
+};
+
+/* Create first node */
+struct node* create_node(int data) {
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->next = temp;
+    temp->prev = temp;
+    return temp;
+}
+
+/* Insert at beginning */
+struct node* ins_at_beg(struct node* tail) {
+    int data;
+    printf("Enter data for insert at beginning: ");
+    scanf("%d", &data);
+
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = data;
+
+    if (tail == NULL) {
+        temp->next = temp;
+        temp->prev = temp;
+        return temp;
+    }
+
+    struct node* first = tail->next;
+
+    temp->next = first;
+    temp->prev = tail;
+    first->prev = temp;
+    tail->next = temp;
+
+    return tail;   // tail does NOT change
+}
+
+/* Insert at end */
+struct node* ins_at_end(struct node* tail) {
+    int data;
+    printf("Enter data for insert at end : ");
+    scanf("%d", &data);
+
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = data;
+
+    if (tail == NULL) {
+        temp->next = temp;
+        temp->prev = temp;
+        return temp;
+    }
+
+    struct node* first = tail->next;
+
+    first->prev = temp;
+    temp->next = first;
+    temp->prev = tail;
+    tail->next = temp;
+
+    tail = temp;
+
+    return tail;   // tail change
+}
+
+/* Insert after position */
+struct node* ins_aft_pos(struct node* tail) {
+    int data,pos;
+    printf("Enter position for insert after : ");
+    scanf("%d", &pos);
+    
+    
+    if (tail == NULL) {
+        printf("List is empty so enter data is 1st node. ");
+        printf("Enter data for insert after position : ");
+        scanf("%d", &data);
+        return create_node(data);
+    }
+    
+    struct node* first = tail->next;
+    
+    if(pos == 1){
+        printf("Enter data for insert after position : ");
+        scanf("%d", &data);
+        
+        struct node* start = tail->next;
+        struct node* temp = (struct node*)malloc(sizeof(struct node));
+        temp->data = data;
+        temp->next = start->next;
+        temp->prev = start;
+        start->next->prev = temp->next;
+        start->next = temp;
+    }
+    else{
+        struct node* start = tail->next;
+        int n=1;
+        while(1 != pos){
+            start = start->next;
+            if(start == tail)
+                return ins_at_end(tail);
+            pos--;
+        }
+        printf("Enter data for insert after position : ");
+        scanf("%d", &data);
+        struct node* temp = (struct node*)malloc(sizeof(struct node));
+        temp->data = data;
+        temp->next = start->next;
+        temp->prev = start;
+        start->next->prev = temp;
+        start->next = temp;
+    }
+    return tail;   // tail does not change
+}
+
+/* Print list */
+void print(struct node* tail) {
+    if (tail == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    struct node* p = tail->next;
+    do {
+        printf("%d ", p->data);
+        p = p->next;
+    } while (p != tail->next);
+    printf("\n");
+}
+
+int main() {
+    struct node* tail = NULL;
+
+    tail = create_node(10);
+    print(tail);
+
+    tail = ins_at_beg(tail);
+    print(tail);
+
+    tail = ins_at_end(tail);
+    print(tail);
+
+    tail = ins_aft_pos(tail);
+    print(tail);
+
+    return 0;
+}
