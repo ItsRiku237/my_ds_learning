460ced51d8241b2298e16605ddcd461a5de8a6e8
Riku Sahu <riku.sahu237@gmail.com>
2026-02-23 22:29:26 +0530

    Add files via upload

---
+++ 12_Sorting/07_Merge_Sort_Time_Analysis.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int *L = (int *)malloc(n1 * sizeof(int));
+    int *R = (int *)malloc(n2 * sizeof(int));
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+
+    free(L);
+    free(R);
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = (l + r) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+int main()
+{
+    int n, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int *arr = (int *)malloc(n * sizeof(int));
+
+    if (arr == NULL)
+    {
+        printf("Memory allocation failed!\n");
+        return 0;
+    }
+
+    // Generate unsorted random numbers
+    srand(time(0));
+    for (i = 0; i < n; i++)
+    {
+        arr[i] = rand() % 10000;
+    }
+
+    printf("\nUnsorted Array:\n");
+    for (i = 0; i < n && i < 20; i++) // limit printing
+        printf("%d ", arr[i]);
+
+    clock_t start = clock();
+
+    mergeSort(arr, 0, n - 1);
+
+    clock_t end = clock();
+
+    printf("\n\nSorted Array:\n");
+    for (i = 0; i < n && i < 20; i++)
+        printf("%d ", arr[i]);
+
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\n\nTime taken: %f seconds\n", time_taken);
+
+    free(arr);
+
+    return 0;
+}
